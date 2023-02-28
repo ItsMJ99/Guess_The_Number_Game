d4adba3953af87c778651fee8ccb627d97e3d8eb
ItsMJ99 <madhav3jadhav@gmail.com>
2023-02-28 17:31:22 +0530

    init

---
+++ code.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <random>
+using namespace std;
+
+void game()
+{
+    int range[2];
+    cout << "Enter range (START END) = ";
+    cin >> range[0] >> range[1];
+
+    if (range[0] >= range[1])
+    {
+        int s = range[0];
+        range[0] = range[1];
+        range[1] = s;
+        cout << "Range Set Successfully......" << endl;
+    }
+}
+
+int main()
+{
+    system("cls");
+    int opt;
+MENU:
+    cout << "<=============> Menu <=============>" << endl;
+    cout << "1. Start Game" << endl;
+    cout << "2. Quit Game" << endl;
+    cout << "\nEnter Choice = ";
+    cin >> opt;
+
+    switch (opt)
+    {
+    case 1:
+        system("cls");
+        game();
+        break;
+
+    case 2:
+        system("cls");
+        exit(0);
+        break;
+
+    default:
+        system("cls");
+        goto MENU;
+    }
+}
